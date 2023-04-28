f6b44aa13398c523f21694fcd96e19a3129b4622
Artem Chumachenko <126257239+Rabbitarts@users.noreply.github.com>
2023-04-28 23:17:33 +0300

    Add files via upload
    Jarvis web-site

---
+++ Jarvis-website/js/SmoothScroll.js
@@ -0,0 +1,12 @@
+SmoothScroll({
+    animationTime    : 800,
+    stepSize         : 75,
+    accelerationDelta : 30,  
+    accelerationMax   : 2,   
+    keyboardSupport   : true,  
+    arrowScroll       : 50,
+    pulseAlgorithm   : true,
+    pulseScale       : 4,
+    pulseNormalize   : 1,
+    touchpadSupport   : true,
+})
